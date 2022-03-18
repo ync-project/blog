578077ee1ff009a8d181bd145aefd0bda33015ee
yongnan <ync@fastmail.com>
2022-03-18 17:17:37 +0800

    apollo_fetchMore_not_work

--- frontend/components/post/PostList.tsx
+++ frontend/components/post/PostList.tsx
@@ -1,15 +1,100 @@
+import { gql, useQuery, NetworkStatus } from '@apollo/client'
 import { useState } from 'react'
 import { Post, Response} from '../../interfaces/graphql_generated'
 import PostItem from './PostItem'
-export type TODOResult = any
+import ErrorMessage from '../../components/error-message'
+import { FeedsDocument} from '../../interfaces/graphql_generated'
 
-export default function Posts({posts} : {posts: Response["posts"]} ) {
+export type TODOResult = any
+export const allPostsQueryVars = {
+  page: 1,
+  take: 3
+}
+
+
+export default function PostList() {
+  const { loading, error, data, fetchMore, networkStatus } = useQuery(
+    FeedsDocument,
+    {
+      variables: allPostsQueryVars,
+      // Setting this value to true will make the component rerender when
+      // the "networkStatus" changes, so we are able to know if it is fetching
+      // more data
+      notifyOnNetworkStatusChange: true,
+    }
+  )
+
+  const loadingMorePosts = networkStatus === NetworkStatus.fetchMore
+
+  const loadMorePosts = () => {
+    fetchMore({
+      variables: {
+        //skip: posts.length,
+        page: currentPage + 1,
+      },
+    })
+  }
+
+  if (error) return <ErrorMessage message="Error loading posts." />
+  if (loading && !loadingMorePosts) return <div>Loading</div>
+
+  const {pageInfo: {hasNextPage, currentPage}, posts} = data.feeds
+
   return (
-    <ul>
-      {posts?.map((post) => (
-        <PostItem key={post?.id} post={post as Post} />
-      ))}
-    </ul>
-  );
+    <section>
+      <ul>
+        {posts.map(({post}: {post: Post}, index: number ) => (
+          <li key={index}>
+            <div>
+                post.id. post.title
+            </div>
+          </li>
+        ))}
+      </ul>
+      {hasNextPage && (
+        <button onClick={() => loadMorePosts()} disabled={loadingMorePosts}>
+          {loadingMorePosts ? 'Loading...' : 'Show More'}
+        </button>
+      )}
+      <style jsx>{`
+        section {
+          padding-bottom: 20px;
+        }
+        li {
+          display: block;
+          margin-bottom: 10px;
+        }
+        div {
+          align-items: center;
+          display: flex;
+        }
+        a {
+          font-size: 14px;
+          margin-right: 10px;
+          text-decoration: none;
+          padding-bottom: 0;
+          border: 0;
+        }
+        span {
+          font-size: 14px;
+          margin-right: 5px;
+        }
+        ul {
+          margin: 0;
+          padding: 0;
+        }
+        button:before {
+          align-self: center;
+          border-style: solid;
+          border-width: 6px 4px 0 4px;
+          border-color: #ffffff transparent transparent transparent;
+          content: '';
+          height: 0;
+          margin-right: 5px;
+          width: 0;
+        }
+      `}</style>
+    </section>
+  )
 }
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,37 +1,32 @@
 import Layout from "../components/Layout"
-import PostList from '../components/post/PostList'
 import ErrorMessage from '../components/error-message'
 import { GetStaticProps } from "next";
-import client from "../lib/apollo-client"; 
-import { Post, FeedsQuery, useFeedsQuery, Response, FeedsDocument} from '../interfaces/graphql_generated'
+import { initializeApollo, addApolloState } from '../lib/apollo-client2'
+import { FeedsDocument }  from '../interfaces/graphql_generated'
+import PostList, {
+  allPostsQueryVars,
+} from '../components/post/PostList'
 
-export const allPostsQueryVars = {
-  page: 1,
-  take: 3
-}
-
-const Home = ({posts} : {posts: Response["posts"]}) => {
+const Home = () => {
   return (
     <Layout>
-      <PostList posts={posts}/>
+      <PostList />
     </Layout>  
   )
 }
 
 export default Home;
 
-export const getStaticProps: GetStaticProps = async () => {
-  const { data, loading, error} = await client.query({
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  await apolloClient.query({
     query: FeedsDocument,
     variables: allPostsQueryVars,
-  });
+  })
 
-  if (error) return <ErrorMessage message="Error loading posts." />
-  if (!data) return <div>Loading</div>
-
-  return {
-    props: { 
-      posts: data.feeds.posts as Response["posts"]
-    },
-  }
+  return addApolloState(apolloClient, {
+    props: {},
+    revalidate: 1,
+  })
 }
