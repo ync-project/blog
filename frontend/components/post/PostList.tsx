e295d0b854fb0f7005f60e1b23d4e80c9667055f
yongnan <ync@fastmail.com>
2022-03-21 08:08:58 +0800

    U

--- frontend/components/post/PostList.tsx
+++ frontend/components/post/PostList.tsx
@@ -4,11 +4,50 @@ export type TODOResult = any
 import client from "../../lib/apollo-client"; 
 import { FeedsDocument} from '../../interfaces/graphql_generated'
 import ReactPaginate from 'react-paginate';
+import styled from 'styled-components';
+
+// You can style your pagination component
+// thanks to styled-components.
+// Use inner class names to style the controls.
+const MyPaginate = styled(ReactPaginate).attrs({
+  // You can redifine classes here, if you want.
+  activeClassName: 'active', // default to "disabled"
+})`
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  list-style-type: none;
+  padding: 0 5rem;
+  li a {
+    border-radius: 7px;
+    padding: 0.1rem 1rem;
+    border: gray 1px solid;
+    cursor: pointer;
+  }
+  li.previous a,
+  li.next a,
+  li.break a {
+    border-color: transparent;
+  }
+  li.active a {
+    background-color: #0366d6;
+    border-color: transparent;
+    color: white;
+    min-width: 32px;
+  }
+  li.disabled a {
+    color: grey;
+  }
+  li.disable,
+  li.disabled a {
+    cursor: default;
+  }
+`;
 
 type Page = {
   selected: number
 }
-
 
 const loadingMorePosts = false
 export default function Posts( { response, pagginationHandler }: 
@@ -29,27 +68,54 @@ export default function Posts( { response, pagginationHandler }:
       );
   }
   return (
+    <>
     <div className="container">
         <h1>Posts List with Pagination in Next.js</h1>
         <div className="posts">
             {content}
         </div>
-
-        <ReactPaginate
-            previousLabel={'previous'}
-            nextLabel={'next'}
-            breakLabel={'...'}
-            breakClassName={'break-me'}
-            activeClassName={'active'}
-            containerClassName={'pagination'}
-            //subContainerClassName={'pages pagination'}
-            initialPage={Number(pageInfo?.currentPage) - 1}
-            pageCount={Number(pageInfo?.pageCount)}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={5}
-            onPageChange={pagginationHandler}
+        <MyPaginate
+          pageCount={20}
+          onPageChange={pagginationHandler}
+          forcePage={pageInfo?.currentPage}
         />
+        <nav aria-label="Page navigation comments" className="mt-4">
+        <ReactPaginate
+            previousLabel="previous"
+            nextLabel="next"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            pageCount={20}
+            pageRangeDisplayed={4}
+            marginPagesDisplayed={2}
+            onPageChange={pagginationHandler}
+            containerClassName="pagination justify-content-center"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            activeClassName="active"
+            // eslint-disable-next-line no-unused-vars
+            hrefBuilder={(page, pageCount, selected) =>
+              page >= 1 && page <= pageCount ? `/page/${page}` : '#'
+            }
+            hrefAllControls
+            forcePage={pageInfo?.currentPage}
+            onClick={(clickEvent) => {
+              console.log('onClick', clickEvent);
+              // Return false to prevent standard page change,
+              // return false; // --> Will do nothing.
+              // return a number to choose the next page,
+              // return 4; --> Will go to page 5 (index 4)
+              // return nothing (undefined) to let standard behavior take place.
+            }}
+          />  
+        </nav>  
     </div>
+    </>
   )
 }
 
