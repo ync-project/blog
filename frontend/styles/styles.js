dd47344cd5000dfd2e046c13146f063550b23476
yongnan <ync@fastmail.com>
2022-05-04 22:20:21 +0800

    updating

--- frontend/styles/styles.js
+++ frontend/styles/styles.js
@@ -169,3 +169,21 @@ export const ChapterList = styled.div`
     width: 100%;
 `
 
+export const VoteButton = styled.button`
+    background-color: transparent;
+    border: 1px solid #e4e4e4;
+    color: #000;
+    :active {
+        background-color: transparent;
+    }
+    :before {
+        align-self: center;
+        border-color: transparent transparent #000000 transparent;
+        border-style: solid;
+        border-width: 0 4px 6px 4px;
+        content: '';
+        height: 0;
+        margin-right: 5px;
+        width: 0;
+    }
+`
